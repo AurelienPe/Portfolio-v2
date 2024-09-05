4242ef142fca3603a1214a6b3669627c79834ca4
PEYRAT Aurélien <aure1814fr@gmail.com>
2024-09-05 05:37:45 +0200

    add html, style and index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import './style.css'
+import './fa.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Home from './pages/home/Home'
+import Footer from './components/footer/Footer'
+import Nav from './components/nav/Nav'
+import Error from './pages/error/Error'
+
+
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/404" element={<Error />} />
+        <Route path="/*" element={<Navigate to="/404" />} /> 
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
